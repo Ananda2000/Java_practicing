2b08f7935c6d4403ffeea7af30152894341b9509
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2023-08-06 21:11:38 +0530

    This is testing 6-aug-2024

--- FileHandling/simpleTextFile/SimpleFile_creation.java
+++ FileHandling/simpleTextFile/SimpleFile_creation.java
@@ -6,22 +6,31 @@ package simpleTextFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class SimpleFile_creation 
 {
 	public static void main(String [] args) throws IOException
 	{
-		File filess = new File("D:\\FileHandlingPRactice\\kumar.txt");
+
+		System.out.println("please enter the file name :");
+		Scanner scn = new Scanner(System.in);
+		String filename = scn.next();
+		File filess = new File("D:\\FileHandlingPRactice\\"+filename);
 		
 		if(filess.exists())
 		{
 			System.out.println("file already present");
 		}
-		else
+		else if (!filess.exists()) 
+			
 		{
 			filess.createNewFile();
 			System.out.println("File created successfully");
 		}
+		else {
+			System.out.println("file not created ---- please check the program");
+		} 
 	}
 
 }
