b52b128a4d26bc1bb7ab77f93c62287e11151660
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2025-08-06 20:28:07 +0530

    This adding program Write a Java Program to remove all white spaces from a string without using replace().

--- com.Java.InterviewPrograms/string_programs/P13.java
+++ com.Java.InterviewPrograms/string_programs/P13.java
@@ -7,16 +7,26 @@ public class P13
 	
 	public static String removeWhiteSpaces(String words)
 	{
+		System.out.println("testing -1");
 		int leng= words.length();
 		String str2="";
 		char [] aa = words.toCharArray();
 		for (int i = 0; i < leng; i++) 
 		{
-			while((aa[i]!=' ') || (aa[i]!='\t'))
+			System.out.println("testing -2");
+	
+			while(aa[i]!=' ' && aa[i]!='\t' )
 			{
+				System.out.println("testing -3");
 				str2=str2+aa[i];
+				System.out.println("testing -4");
+				if(i==leng)
+				{
+					break;
+				}
+				i++;
 			}
-			System.out.println("This is testing");
+			System.out.println("testing -5");
 		}
 		
 		return str2;
@@ -27,7 +37,8 @@ public class P13
 	
 	public static void main(String [] args)
 	{
-		String str ="remove white spaces without using replace method";
+		String str ="re m o   ve whi		te ";
+		System.out.println("testing");
 		String removed =removeWhiteSpaces(str);
 		System.out.println("The removed string is ");
 		System.out.println(removed);
